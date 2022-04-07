d9a5487828f67e850fe87eabc3790259c1349329
oubin <oubin17@gmail.com>
2022-04-07 20:32:01 +0800

    print

---
+++ print.py
@@ -0,0 +1,12 @@
+# a+ 文件不存在，则创建，存在，则追加
+fp=open('/Users/oubin/document/PythonProject/first_python/test.txt', "a+")
+print("hello world", file=fp)
+fp.close()
+
+print("hello\nworld")
+print("hello\tworld")
+print("hellooo\tworld")
+print("hello\rworld")#覆盖
+print("hello\bworld")#退格
+
+print(r"hellooo\tworld")#转义字符不起作用，在字符串前加r或R
