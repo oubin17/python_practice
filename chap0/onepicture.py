cab7c4caaff6d713accc6718ba84f451730ea5c7
oubin <oubin17@gmail.com>
2022-08-20 23:16:23 +0800

    test

---
+++ chap0/onepicture.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/20 22:23
+# @Author  : oubin
+# @File    : onepicture.py
+# @Software: PyCharm
+import os
+
+
+def main(name):
+    print("hello, %s" % name)
+    print(f'hello, {name}')
+
+
+if __name__ == '__main__':
+    main("123")
--- chap1/main.py
+++ chap1/main.py
@@ -2,11 +2,14 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi,{name}')  # Press ⌘F8 to toggle the breakpoint.
+    print("hi,%s" % name)
+    print("当前目录为：" + os.getcwd())
 
 
 # Press the green button in the gutter to run the script.
